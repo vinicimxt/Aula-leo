e3771edaa94bfa4530dccb92c2be4000c89aa260
vinicius <vinicius.generoso2@etec.sp.gov.br>
2024-05-21 19:27:15 -0300

    21-05

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Title from './src/components/Title';
+import Form from './src/components/Form';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen
+    //       name='Home'
+    //       component={HomeScreen}
+    //     />
+    //     <Stack.Screen
+    //       name='Detalhes'
+    //       component={DetailsScreen}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    <View style={styles.container}>
+      <Title/>
+      <Form/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e0e5e5',
+    paddingTop:80
+  },
+});
